bc732f8d50e9a94406f73e285193486d06195df3
wijesekarawmepbb <129168716+PrabashwaraBBWijesekara@users.noreply.github.com>
2023-08-25 09:12:07 +0530

    previos main.cpp crashed new one added

--- main.cpp
+++ main.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
 
+class OrderBookEntry {
+public:
+    std::string ID;
+    std::string instrument;
+    int side;
+    double price;
+    int quantity;
+
+    OrderBookEntry(std::string ID, std::string instrument, int side, double price, int quantity)
+        : ID(ID), instrument(instrument), side(side), price(price), quantity(quantity) {
+    }
+};
+
+std::vector<std::string> split(const std::string &s, char delimiter) {
+    std::vector<std::string> tokens;
+    std::string token;
+    std::istringstream tokenStream(s);
+    while (std::getline(tokenStream, token, delimiter)) {
+        tokens.push_back(token);
+    }
+    return tokens;
+}
+
+int main() {
+    std::ifstream file("ex7.csv");
+    if (!file.is_open()) {
+        std::cerr << "Could not open the file." << std::endl;
+        return 1;
+    }
+    std::string line;
+
+    std::getline(file, line);
+
+    std::cout << line << std::endl;
 
     std::vector<std::string> fields = split(line, ',');
 
@@ -7,8 +45,7 @@
         std::cout << field << std::endl;
     }
 
-int main()
-{
+    file.close();
 
     return 0;
 }
